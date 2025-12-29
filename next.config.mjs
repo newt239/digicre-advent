e99dc930faaf73aa56436d49576d61ef68d43d33
newt <ysti2019.win2020.st@gmail.com>
2025-12-29 16:56:28 +0900

    fix: wrangler config

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
+  images: { unoptimized: true },
 }
 
 export default nextConfig
